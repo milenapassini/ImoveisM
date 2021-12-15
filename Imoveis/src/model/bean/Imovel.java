5875d834dca2474061b9c55fde8a3038ea0fc126
Arquivos-PC <Arquivos-PC@Arquivos-PC>
2021-12-15 10:54:52 -0300

    Imovel.java - Modelo Bean

---
+++ Imoveis/src/model/bean/Imovel.java
@@ -0,0 +1,97 @@
+package model.bean;
+
+public class Imovel {
+	
+	private int idImovel;
+	private String endereco;
+	private int numero;
+	private String bairro;
+	private String cidade;
+	private int CEP;
+	private boolean venda;
+	private boolean doublevalor;
+	
+	
+	public int getIdImovel() {
+		return idImovel;
+	}
+	public void setIdImovel(int idImovel) {
+		this.idImovel = idImovel;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public int getCEP() {
+		return CEP;
+	}
+	public void setCEP(int cEP) {
+		CEP = cEP;
+	}
+	public boolean isVenda() {
+		return venda;
+	}
+	public void setVenda(boolean venda) {
+		this.venda = venda;
+	}
+	public boolean isDoublevalor() {
+		return doublevalor;
+	}
+	public void setDoublevalor(boolean doublevalor) {
+		this.doublevalor = doublevalor;
+		
+	}
+
+	
+	public boolean getValor() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	public boolean getVenda() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	public void setNumero(String text) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setCEP(String text) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setVenda(String text) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setValor(String text) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setValor(boolean boolean1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
